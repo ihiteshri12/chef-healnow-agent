dc0fae5442bbd5a0f295cf02f0224a0d703dc2eb
Noel Georgi <noel.georgi@reancloud.com>
2016-12-29 21:45:39 +0530

    Using IAM role instead of access keys and providing hardcoded loation of bucket

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,6 @@
 
-default['healnow-agent']['deb_s3_bucket'] = ''
+default['healnow-agent']['deb_s3_bucket'] = 'dnow-test'
 default['healnow-agent']['deb_s3_bucket_region'] = 'us-east-1'
-default['healnow-agent']['deb_s3_key'] = ''
-default['healnow-agent']['deb'] = ''
+default['healnow-agent']['deb_s3_key'] = 'jenkins/healnow/ubuntu'
+default['healnow-agent']['deb'] = 'healnow-client_0.0.0+20161216101330-1_amd64.deb'
 default['healnow-agent']['healnow_server_ip'] = ''
-default['healnow-agent']['aws_s3_access_key'] = ''
-default['healnow-agent']['aws_s3_secret_key'] = ''
